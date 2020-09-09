7d3dbca92a2003ade22e7c9f3689bc67952373e4
cesar caceres <cesarc770@gmail.com>
2020-09-09 15:21:22 -0600

    added opeartor overloads to Point2D

--- Engine/Math/Point2D.cpp
+++ Engine/Math/Point2D.cpp
@@ -12,3 +12,66 @@
 		Y = y;
 	}
 
+	bool Point2D::operator==(const Point2D& P) const
+	{
+		return (X == P.X) && (Y == P.Y);
+	}
+
+	bool Point2D::operator!=(const Point2D& P) const
+	{
+		return !((X == P.X) && (Y == P.Y));
+	}
+
+	Point2D Point2D::operator+ (const Point2D& P) const
+	{
+		return Point2D(X + P.X, Y + P.Y);
+	}
+
+	Point2D Point2D::operator-(const Point2D& P) const
+	{
+		return Point2D(X - P.X, Y - P.Y);
+	}
+
+	Point2D Point2D::operator*(const int& num) const
+	{
+		return Point2D(X * num, Y * num);
+	}
+
+	Point2D Point2D::operator/(const int& num) const
+	{
+		return Point2D(X / num, Y / num);
+	}
+
+	Point2D Point2D::operator-() const
+	{
+		return Point2D(-X, -Y);
+	}
+
+	Point2D Point2D::operator+=(const Point2D& P)
+	{
+		X += P.X;
+		Y += P.Y;
+		return *this;
+	}
+
+	Point2D Point2D::operator-=(const Point2D& P)
+	{
+		X -= P.X;
+		Y -= P.Y;
+		return *this;
+	}
+
+	Point2D Point2D::operator*=(const int& num)
+	{
+		X *= num;
+		Y *= num;
+		return *this;
+	}
+
+	Point2D Point2D::operator/=(const int& num)
+	{
+		X /= num;
+		Y /= num;
+		return *this;
+	}
+
--- Engine/Math/Point2D.h
+++ Engine/Math/Point2D.h
@@ -7,4 +7,17 @@
 		Point2D();
 		Point2D(int x, int y);
 		int X, Y;
+
+		//operators overload
+		bool operator== (const Point2D& P) const;
+		bool operator!= (const Point2D& P) const;
+		Point2D operator+ (const Point2D& P) const;
+		Point2D operator- (const Point2D& P) const;
+		Point2D operator* (const int& num) const;
+		Point2D operator/ (const int& num) const;
+		Point2D operator- () const;
+		Point2D operator+= (const Point2D& P);
+		Point2D operator-= (const Point2D& P);
+		Point2D operator*= (const int& num);
+		Point2D operator/= (const int& num);
 	};
